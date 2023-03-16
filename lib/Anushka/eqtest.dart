72e74d603240d4d298eec43f59999065696bb15b
anushkaaJ26 <anushka.jadhav1@walchandsangli.ac.in>
2023-03-16 16:48:40 +0530

    EQtestPush

---
+++ lib/Anushka/eqtest.dart
@@ -0,0 +1,181 @@
+import 'package:flutter/material.dart';
+
+import 'data/question.dart';
+
+class QuizScreen extends StatefulWidget {
+  @override
+  State<QuizScreen> createState() => _QuizScreenState();
+}
+
+class _QuizScreenState extends State<QuizScreen> {
+  List<Question> questionList = getQuestions();
+  int currentQuestionIndex = 0;
+  int score = 0;
+  Answer? selectedAnswer;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 32),
+        child:
+            Column(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+          const Text(
+            "Simple Quiz App",
+            style: TextStyle(
+              color: Color(0xFFAA77FF),
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          _questionWidget(),
+          _answerList(),
+          _nextButton(),
+        ]),
+      ),
+    );
+  }
+
+  _questionWidget() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          "Question ${currentQuestionIndex + 1}/${questionList.length.toString()}",
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        const SizedBox(height: 20),
+        Container(
+          alignment: Alignment.center,
+          width: double.infinity,
+          padding: const EdgeInsets.all(32),
+          decoration: BoxDecoration(
+            color: Colors.orangeAccent,
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Text(
+            questionList[currentQuestionIndex].questionText,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  _answerList() {
+    return Column(
+      children: questionList[currentQuestionIndex]
+          .answerList
+          .map(
+            (e) => _answerButton(e),
+          )
+          .toList(),
+    );
+  }
+
+  Widget _answerButton(Answer answer) {
+    bool isSelected = answer == selectedAnswer;
+
+    return Container(
+      width: double.infinity,
+      margin: const EdgeInsets.symmetric(vertical: 8),
+      height: 48,
+      child: ElevatedButton(
+        child: Text(answer.answerText),
+        style: ElevatedButton.styleFrom(
+          shape: const StadiumBorder(),
+          primary: isSelected ? Colors.orangeAccent : Colors.white,
+          onPrimary: isSelected ? Colors.white : Colors.black,
+        ),
+        onPressed: () {
+          if (selectedAnswer == null) {
+            if (answer.isCorrect) {
+              score++;
+            }
+            setState(() {
+              selectedAnswer = answer;
+            });
+          }
+        },
+      ),
+    );
+  }
+
+  _nextButton() {
+    bool isLastQuestion = false;
+    if (currentQuestionIndex == questionList.length - 1) {
+      isLastQuestion = true;
+    }
+
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.5,
+      height: 48,
+      child: ElevatedButton(
+        child: Text(isLastQuestion ? "Submit" : "Next"),
+        style: ElevatedButton.styleFrom(
+          shape: const StadiumBorder(),
+          primary: Colors.blueAccent,
+          onPrimary: Colors.white,
+        ),
+        onPressed: () {
+          if (isLastQuestion) {
+            //display score
+
+            showDialog(context: context, builder: (_) => _showScoreDialog());
+          } else {
+            //next question
+            setState(() {
+              selectedAnswer = null;
+              currentQuestionIndex++;
+            });
+          }
+        },
+      ),
+    );
+  }
+
+  _showScoreDialog() {
+    bool isPassed = false;
+    String title;
+
+    if (score >= questionList.length * 0.65) {
+      //pass if 40 %
+      //isPassed = true;
+
+      title = "You Are Absoutely Fine!";
+    } else if (score < questionList.length * 0.65) {
+      title = "You Are well but you need to have some Meditation in your DailyRoutine!";
+    } else {
+      title = "You need to focus on your Mental Health!";
+    }
+    //String title = isPassed ? "You are  " : "Failed";
+
+    return AlertDialog(
+      title: Text(
+        title + " | Score is $score",
+        style: TextStyle(color: isPassed ? Colors.green : Colors.redAccent),
+      ),
+      content: ElevatedButton(
+        child: const Text("Restart"),
+        onPressed: () {
+          Navigator.pop(context);
+          setState(() {
+            currentQuestionIndex = 0;
+            score = 0;
+            selectedAnswer = null;
+          });
+        },
+      ),
+    );
+  }
+}
