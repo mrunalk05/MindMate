cea4e68d1f8c2385212197f64755c7b9f6c264f7
mrunal <mrunalkhade35@gmail.com>
2023-03-16 21:25:42 +0530

    integration done

---
+++ lib/mrunal/lib/loginPage.dart
@@ -0,0 +1,130 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import '../../MIND/MindMate/lib/mrunal/lib/dashboard.dart';
+import '../../MIND/MindMate/lib/mrunal/lib/registration.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:velocity_x/velocity_x.dart';
+import 'applogo.dart';
+import 'package:http/http.dart' as http;
+import 'config.dart';
+
+class SignInPage extends StatefulWidget {
+  @override
+  _SignInPageState createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  bool _isNotValidate = false;
+  late SharedPreferences prefs;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    initSharedPref();
+  }
+
+  void initSharedPref() async{
+    prefs = await SharedPreferences.getInstance();
+  }
+
+  void loginUser() async{
+    if(emailController.text.isNotEmpty && passwordController.text.isNotEmpty){
+
+      var reqBody = {
+        "email":emailController.text,
+        "password":passwordController.text
+      };
+
+      var response = await http.post(Uri.parse(login),
+          headers: {"Content-Type":"application/json"},
+          body: jsonEncode(reqBody)
+      );
+
+      var jsonResponse = jsonDecode(response.body);
+      if(jsonResponse['status']){
+          var myToken = jsonResponse['token'];
+          prefs.setString('token', myToken);
+          Navigator.push(context, MaterialPageRoute(builder: (context)=>Dashboard(token: myToken)));
+      }else{
+        print('Something went wrong');
+      }
+
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [Color.fromARGB(255, 239, 235, 234),Color.fromARGB(255, 244, 239, 237)],
+                begin: FractionalOffset.topLeft,
+                end: FractionalOffset.bottomCenter,
+                stops: [0.0,0.8],
+                tileMode: TileMode.mirror
+            ),
+          ),
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  
+                  HeightBox(10),
+                  "Email Sign-In".text.size(22).yellow100.make(),
+
+                  TextField(
+                    controller: emailController,
+                    keyboardType: TextInputType.text,
+                    decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        hintText: "Email",
+                        errorText: _isNotValidate ? "Enter Proper Info" : null,
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10.0)))),
+                  ).p4().px24(),
+                  TextField(
+                    controller: passwordController,
+                    keyboardType: TextInputType.text,
+                    decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        hintText: "Password",
+                        errorText: _isNotValidate ? "Enter Proper Info" : null,
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10.0)))),
+                  ).p4().px24(),
+                  GestureDetector(
+                    onTap: (){
+                        loginUser();
+                    },
+                    child: HStack([
+                      VxBox(child: "Login".text.white.makeCentered().p16()).green600.roundedLg.make(),
+                    ]),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        bottomNavigationBar: GestureDetector(
+          onTap: (){
+            Navigator.push(context, MaterialPageRoute(builder: (context)=>Registration()));
+          },
+          child: Container(
+              height: 25,
+              color: Colors.lightBlue,
+              child: Center(child: "Create a new Account..! Sign Up".text.white.makeCentered())),
+        ),
+      ),
+    );
+  }
+}
