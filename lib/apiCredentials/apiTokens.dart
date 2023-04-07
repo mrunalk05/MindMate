a50c5078cd9f4fdf3c7dc2976c7cb842aed9813c
Mrunal Khade <83811490+mrunalk05@users.noreply.github.com>
2023-04-07 13:14:59 +0530

    Updated Api token

--- lib/apiCredentials/apiTokens.dart
+++ lib/apiCredentials/apiTokens.dart
@@ -1,3 +1,3 @@
-//const apiToken = "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";
-const apiToken = "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";
+const apiToken = "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";
+// const apiToken = "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";
 var summary = [];
